ac54540518b24b785d9262fe54384cb5eb520a22
Lifanova <iramilka123@gmail.com>
2020-08-21 13:20:19 +0300

    add mvc version

---
+++ rest-service-mvc/src/main/java/com/example/restservicemvc/RestServiceMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicemvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceMvcApplication.class, args);
+	}
+
+}
---
+++ rest-service-mvc/src/test/java/com/example/restservicemvc/RestServiceMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservicemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
